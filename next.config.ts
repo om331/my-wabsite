b068e53102a95fa7aaa475f9f77e66c1a992afc8
om331 <ompawar8444@gmail.com>
2025-01-18 12:15:54 +0530

    modified next config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output:'export',
+  // output:'export',
   basePath: "/testing-app",
   //exportTrailingSlash: true, // Add trailing slashes to all URL
   images :{
